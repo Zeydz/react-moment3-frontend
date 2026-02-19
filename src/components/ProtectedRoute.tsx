eec9cc05ebaffa422da2c1bdc56d229ab9a7f250
Jocke <joakim03moller@gmail.com>
2026-02-19 16:16:38 +0100

    missed commit

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../store/useAuth";
 import type { ReactNode } from "react";
 
-export default function ProtectedRoute({ children }: { children: ReactNode}) {
+function ProtectedRoute({ children }: { children: ReactNode}) {
     const { user } = useAuth();
 
     if (!user) {
@@ -11,3 +11,4 @@ export default function ProtectedRoute({ children }: { children: ReactNode}) {
 
     return children
 }
+export default ProtectedRoute
